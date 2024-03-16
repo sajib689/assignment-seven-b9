953cbaa70cd35b91db23c4f33dd41b31da8c859d
sajib689 <jibonraja11@gmail.com>
2024-03-16 16:17:04 +0600

    almost done

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -6,6 +6,7 @@ const SideBar = ({ cooks,handlePreparing,prepared }) => {
     const caloriesMap = prepared.map(calo => {return calo.calories})
     const totalTime = costMap.reduce((a,b) => a+b,0)
     const totalCalories = caloriesMap.reduce((a,b) => a+b,0)
+    
   return (
     <div className="mt-20 border rounded-lg md:w-[450px]">
       <h1 className="text-center mt-3 font-bold text-[#282828] text-[24px]">
