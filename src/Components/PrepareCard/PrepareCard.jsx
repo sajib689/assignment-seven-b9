d0829ba696592bf17fe4a29f7ed605397eb9e58d
sajib689 <jibonraja11@gmail.com>
2024-03-16 15:20:39 +0600

    done the second table

---
+++ src/Components/PrepareCard/PrepareCard.jsx
@@ -0,0 +1,21 @@
+
+const PrepareCard = ({prepare,index}) => {
+    const {
+        recipe_name,
+        preparing_time,
+        calories,
+      } = prepare;
+    return (
+        <>
+            <tr>
+              <th>{index+1}</th>
+              <td>{recipe_name}</td>
+              <td>{preparing_time}</td>
+              <td>{calories}</td>
+              
+            </tr>  
+        </>
+    );
+};
+
+export default PrepareCard;
--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -1,3 +1,4 @@
+import PrepareCard from "../PrepareCard/PrepareCard";
 import SideCard from "../SideCard/SideCard";
 
 const SideBar = ({ cooks,handlePreparing,prepared }) => {
@@ -27,6 +28,24 @@ const SideBar = ({ cooks,handlePreparing,prepared }) => {
         </table>
         <hr />
             <h1 className="text-center mt-3 font-bold text-[#282828] text-[24px]">Currently cooking: {prepared.length}</h1>
+            <table className="table">
+          {/* head 2*/}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Time</th>
+              <th>Calories</th>
+            </tr>
+          </thead>
+          <tbody>
+           {
+            prepared.map((prepare,index) => <PrepareCard key={prepare.recipe_id} index={index} prepare={prepare}></PrepareCard>)
+           }
+            
+          </tbody>
+        </table>
+        <hr />
       </div>
     </div>
   );
